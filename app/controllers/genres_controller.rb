e96bee71765c78cd025813bd8bf8ba9c4e69ff4a
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-01-16 01:52:54 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,28 +1,28 @@
 class GenresController < ApplicationController
 
   def index
-    @genre = Artist.new
+    @genre = Genre.new
   end
 
   def new
-    @genre = Artist.new
+    @genre = Genre.new
   end
 
   def create
-    @genre = Artist.create(genre_params(:name, :bio))
+    @genre = Genre.create(genre_params(:name, :bio))
     redirect_to artist_path(@genre)
   end
 
   def show
-    @genre = Artist.find_by(params[:id])
+    @genre = Genre.find_by(params[:id])
   end
 
   def edit
-    @genre = Artist.find_by(params[:id])
+    @genre = Genre.find_by(params[:id])
   end
 
   def update
-    @genre = Artist.find_by(params[:id])
+    @genre = Genre.find_by(params[:id])
     @genre.update(genre_params(:name, :bio))
     redirect_to artist_path(@genre)
   end
