c3e6bfc3b9311d9e5e61d2d961f13499f2d65100
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-01-16 01:37:15 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,21 @@
 class ArtistsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+  
 end
